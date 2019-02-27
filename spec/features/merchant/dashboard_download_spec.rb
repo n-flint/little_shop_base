d93a9e5c98c470d853b31188fa18d6ea4d86d7de
Noah Flint <noah.flint24@gmail.com>
2019-02-26 18:58:21 -0700

    changed setup method for download test

--- spec/features/merchant/dashboard_download_spec.rb
+++ spec/features/merchant/dashboard_download_spec.rb
@@ -2,18 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'merchant customer list download' do
   before :each do
-    @merchant = create(:merchant)
-    @admin = create(:admin)
-    @i1, @i2 = create_list(:item, 2, user: @merchant)
-    @o1, @o2 = create_list(:order, 2)
-    @o3 = create(:completed_order)
-    @o4 = create(:cancelled_order)
-    create(:order_item, order: @o1, item: @i1, quantity: 1, price: 2)
-    create(:order_item, order: @o1, item: @i2, quantity: 2, price: 2)
-    create(:order_item, order: @o2, item: @i2, quantity: 4, price: 2)
-    create(:order_item, order: @o3, item: @i1, quantity: 4, price: 2)
-    create(:order_item, order: @o4, item: @i2, quantity: 5, price: 2)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+      @u1 = create(:user, state: "CO", city: "Fairfield")
+      @u2 = create(:user, state: "OK", city: "OKC")
+      @u3 = create(:user, state: "IA", city: "Fairfield")
+      u4 = create(:user, state: "IA", city: "Des Moines")
+      u5 = create(:user, state: "IA", city: "Des Moines")
+      u6 = create(:user, state: "IA", city: "Des Moines")
+      @m1 = create(:merchant)
+      @m2 = create(:merchant)
+      @i1 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i2 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i3 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i4 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i5 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i6 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i7 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i9 = create(:item, merchant_id: @m1.id, inventory: 20)
+      @i8 = create(:item, merchant_id: @m2.id, inventory: 20)
+      o1 = create(:completed_order, user: @u1)
+      o2 = create(:completed_order, user: @u2)
+      o3 = create(:completed_order, user: @u3)
+      o4 = create(:completed_order, user: @u1)
+      o5 = create(:cancelled_order, user: u5)
+      o6 = create(:completed_order, user: u6)
+      @oi1 = create(:order_item, item: @i1, order: o1, quantity: 2, created_at: 1.days.ago)
+      @oi2 = create(:order_item, item: @i2, order: o2, quantity: 7, created_at: 7.days.ago)
+      @oi3 = create(:order_item, item: @i2, order: o3, quantity: 7, created_at: 7.days.ago)
+      @oi4 = create(:order_item, item: @i3, order: o3, quantity: 4, created_at: 6.days.ago)
+      @oi5 = create(:order_item, item: @i4, order: o4, quantity: 3, created_at: 4.days.ago)
+      @oi6 = create(:order_item, item: @i5, order: o5, quantity: 1, created_at: 5.days.ago)
+      @oi7 = create(:order_item, item: @i6, order: o6, quantity: 2, created_at: 3.days.ago)
+      @oi1.fulfill
+      @oi2.fulfill
+      @oi3.fulfill
+      @oi4.fulfill
+      @oi5.fulfill
+      @oi6.fulfill
+      @oi7.fulfill
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m1)
+      visit dashboard_path
+
 
   end
 
